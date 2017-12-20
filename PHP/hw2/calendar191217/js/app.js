e9b9c8f7679fac058ba21c6381497fe1e4b2d033
Kuznetsov Sergei <segakuz@gmail.com>
2017-12-20 19:55:35 +0300

    php hw2

--- PHP/hw2/calendar191217/js/app.js
+++ PHP/hw2/calendar191217/js/app.js
@@ -1,4 +1,4 @@
-let dt = '19.12.2017';
+let dt = '019.12.2017';
 
 var request = $.ajax({
     method: "POST",
@@ -9,10 +9,17 @@ var request = $.ajax({
     dataType: "json"
 });
 
+/* первый способ 
 request.done(function (msg) {
-    $('#log').html(msg.header + "<img src=" + msg.img + ">" + msg.desc);
+    $('#day-calendar').html("<h1>" + msg.date + "</h1>" + "<h2>" + msg.header + "</h2>" + "<img src=" + msg.img + ">" + "<p>" + msg.desc + "</p>");
+});
+*/
+/* второй способ */
+request.done(function (msg) {
+    $('#day-calendar').html("<h1>" + msg[0] + "</h1>" + "<h2>" + msg[1] + "</h2>" + "<img src=" + msg[3] + ">" + "<p>" + msg[2] + "</p>");
 });
 
 request.fail(function (jqXHR, textStatus) {
     alert('Request failed: ' + textStatus);
 });
+/**/
--- PHP/hw2/calendar191217/servicer.php
+++ PHP/hw2/calendar191217/servicer.php
@@ -1,6 +1,12 @@
 <?php
 
-
+/* первый способ 
 echo file_get_contents('tmpl/'.$_POST["id"].".txt", FILE_USE_INCLUDE_PATH);
+*/
+/* второй способ */
+$str = file_get_contents('./tmpl/19.12.2017.txt', FILE_USE_INCLUDE_PATH);
+$arr = explode(";", $str);
+echo json_encode($arr);
+
 
 ?>
