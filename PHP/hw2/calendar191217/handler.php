53768b55b4191aaec709d7df54f6fc14288be037
Kuznetsov Sergei <segakuz@gmail.com>
2017-12-24 12:46:03 +0300

    +1

--- PHP/hw2/calendar191217/handler.php
+++ PHP/hw2/calendar191217/handler.php
@@ -1,19 +1,36 @@
 <?php
 
+/*
 $date = $_POST["date"];
 $arr = explode("-", $date);
 $currDate = $arr[2].".".$arr[1].".".$arr[0];
+*/
+
+$currDate = date('d.m.Y', strtotime($_POST['date'])); //от Даниила
 
 $header = $_POST["header"];
 $desc = $_POST["desc"];
-$img = $currDate . ".jpg";
+$img = "img/" . $currDate . ".jpg";
 
-$str = $currDate.";".$header.";".$desc.";img/".$img;
+// первый способ
+//$str = $currDate.";".$header.";".$desc.";img/".$img;
+
+
+
+// второй способ
+$arr1 = [
+    "date" => $currDate, 
+    "header" => $header,
+    "desc" => $desc,
+    "img" => $img
+    ];
+
+$str = json_encode($arr1);
 
 $writing = file_put_contents("tmpl/".$currDate.".txt", $str, FILE_USE_INCLUDE_PATH);
 
 if($writing) {
-    echo "Успешно записана следующая информация: " . $str . " - в файл: " . "tmpl/".$currDate.".txt";
+    echo "Успешно записана следующая информация: " . $str . " - в файл: " . "tmpl/".$currDate.".txt<br><a href=\"admin.php\">Вернуться в админку</a>";
 } else {
     echo "Что-то пошло не так!";
 }
